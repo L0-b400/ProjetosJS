8e802213c53ece10912d758541215f8e530fa748
Lbzin <llane7203@gmail.com>
2025-04-16 00:20:10 -0300

    v1.3

--- Mata mosquito/jogo.js
+++ Mata mosquito/jogo.js
@@ -11,17 +11,33 @@ ajustaJogo()
 
 function posRandom() {
 
-var posx = Math.floor(Math.random() * alt)
-var posy = Math.floor(Math.random() * lar)
+var posx = Math.floor(Math.random() * alt) - 90
+var posy = Math.floor(Math.random() * lar) - 90
+
+posx = posx < 0 ? 0 : posx
+posy = posy < 0 ? 0 : posy
 
 console.log(posx, posy)
 
 var mosca = document.createElement('img')
 mosca.src = 'imagens/mosca.png' 
-mosca.className = 'mosca'
+mosca.className = tmnMosca()
 mosca.style.left = posx + 'px'
 mosca.style.top = posy + 'px'
 mosca.style.position = 'absolute'
 document.body.appendChild(mosca)
 
+}
+
+function tmnMosca() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch  (classe) {
+        case 0:
+            return 'mosca1'
+        case 1:
+            return 'mosca2'
+        case 2:
+            return 'mosca3' 
+    }
 }
